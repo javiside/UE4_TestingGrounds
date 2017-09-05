4446ecb536d77be5f6802c330edb8368f64d37df
Francisco Javier Martinez <javi.mtz@hotmail.com>
2017-09-05 07:06:13 -0700

    056 Refactored Fire event (TODO fix super rapid fire from AI)

--- Source/TestingGrounds/Character/CharacterCPP.cpp
+++ Source/TestingGrounds/Character/CharacterCPP.cpp
@@ -39,24 +39,27 @@ void ACharacterCPP::BeginPlay()
 	Gun = GetWorld()->SpawnActor<AGun>(GunBlueprint);
 	Gun->AttachToComponent(Mesh1P, FAttachmentTransformRules(EAttachmentRule::SnapToTarget, true), TEXT("GripPoint")); //Attach gun mesh component to Skeleton, doing it here because the skelton is not yet created in the constructor
 	Gun->AnimInstance = Mesh1P->GetAnimInstance();
+
+	if (InputComponent)
+	{
+		InputComponent->BindAction("Fire", IE_Pressed, this, &ACharacterCPP::PullTrigger);
+	}
 }
 
 // Called every frame
 void ACharacterCPP::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
-
 }
 
 // Called to bind functionality to input
 void ACharacterCPP::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
-
 }
 
 
-void ACharacterCPP::Fire()
+void ACharacterCPP::PullTrigger()
 {
 	Gun->OnFire();
 }
